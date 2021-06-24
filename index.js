32b6cd0c4a0e0ef323c245a03a600562970e9cfc
Toshi <tosmow@gmail.com>
2021-06-24 11:17:41 -0600

    Updated indrex

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = require("./server");
 const persist = require("./persist");
 
 //define a port
-const port = process.argv[2] || 8080;
+const port = process.argv[2] || process.env.PORT || 8080;
 
 persist.connect(()=>{
     //start the server
